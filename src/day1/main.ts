1d3e084f375e9402311557312d786f6b83496af9
Gabe Dunn <gabe@sent.at>
2022-12-13 19:45:52 -0800

    add day 2 part 2

--- src/day1/main.ts
+++ src/day1/main.ts
@@ -2,9 +2,9 @@ const readFile = async (): Promise<Array<string>> => {
 	return (await Deno.readTextFile(Deno.cwd() + '/src/day1/input.txt')).split('\n')
 }
 
-const splitFile = (lines: Array<string>): Array<Array<string>> => {
-	const elves: Array<Array<string>> = []
-	let currentElf: Array<string> = []
+const splitFile = (lines: string[]): string[][] => {
+	const elves: string[][] = []
+	let currentElf: string[] = []
 
 	lines.forEach(line => {
 		if (line === '') {
@@ -18,8 +18,8 @@ const splitFile = (lines: Array<string>): Array<Array<string>> => {
 	return elves
 }
 
-const getElfTotals = (elves: Array<Array<string>>): Array<number> => {
-	return elves.map((elf: Array<string>): number => {
+const getElfTotals = (elves: string[][]): Array<number> => {
+	return elves.map((elf: string[]): number => {
 		return elf.map(e => parseInt(e))
 		.reduce((sum, val) => {
 			return sum + val
@@ -27,17 +27,40 @@ const getElfTotals = (elves: Array<Array<string>>): Array<number> => {
 	})
 }
 
-const findTopElf = (elves: Array<number>): number => {
+const findTopElf = (elves: number[]): number => {
 	return elves.reduce((max, curr) => Math.max(max, curr))
+}
+
+const findTopThreeElves = (elves: number[]): number => {
+	const top: number[] = []
+
+	elves.forEach(elf => {
+		top.sort((a, b) => a - b).reverse()
+		if (top.length < 3) {
+			top.push(elf)
+		} else {
+			if (elf > top[2]) {
+				top.pop()
+				top.push(elf)
+			}
+		}
+	})
+
+	console.log(top)
+
+	return top.reduce((sum, val) => sum + val)
 }
 
 const main = async () => {
 	const input = await readFile()
 	const rawElfData = splitFile(input)
 	const totalElfData = getElfTotals(rawElfData)
-	const topElf = findTopElf(totalElfData)
 
-	console.log(`Solution: ${topElf}`)
+	const topElf = findTopElf(totalElfData)
+	console.log(`Day 1 Pt. 1 Solution: ${topElf}`)
+
+	const topThreeElves = findTopThreeElves(totalElfData)
+	console.log(`Day 1 Pt. 2 Solution: ${topThreeElves}`)
 }
 
 main()
